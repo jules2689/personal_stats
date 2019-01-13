b929e36c34773099baf0d50344bc132e0352cb94
Julian Nadeau <julian@jnadeau.ca>
2019-01-13 18:56:47 -0500

    Split things up more, aggregate stats for slack, graph stats v0

--- database.rb
+++ database.rb
@@ -3,6 +3,18 @@ require "sqlite3"
 class Database
   attr_accessor :db
 
+  def self.format_time(t)
+    t = case t
+    when Time
+      t
+    when t =~ /\d+/
+      Time.at(t.to_f)
+    else
+      Time.parse(t)
+    end
+    t.strftime("%Y-%m-%d %H:%M:%S")
+  end
+
   def initialize
     @db = SQLite3::Database.new("messages.db")
     create_tables
@@ -12,6 +24,10 @@ class Database
     cols = values.keys.join(',')
     q_marks = values.keys.map { |_| '?' }.join(',')
     @db.execute("INSERT OR IGNORE INTO #{table} (#{cols}) VALUES (#{q_marks})", values.values)
+  end
+
+  def slack_channels
+    @db.execute("SELECT id, name from user_channels").to_h
   end
 
   def stats
@@ -60,6 +76,25 @@ class Database
         messages_sent BIGINT,
         recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
       );
+    SQL
+
+    @db.execute <<-SQL
+      create table if not exists aggregate_stats (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+        channel_id varchar(16),
+        type varchar(16),
+        messages_sent BIGINT,
+        for_date TIMESTAMP NOT NULL,
+        recorded_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+      );
+    SQL
+
+    @db.execute <<-SQL
+      CREATE INDEX IF NOT EXISTS aggregate_stats_for_date_index ON aggregate_stats (for_date);
+    SQL
+
+    @db.execute <<-SQL
+      CREATE UNIQUE INDEX IF NOT EXISTS aggregate_stats_channel_id_for_date_index ON aggregate_stats (channel_id, for_date);
     SQL
   end
 end
